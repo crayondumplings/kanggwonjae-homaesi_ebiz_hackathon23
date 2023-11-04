dae44ccf9d0094554d292f3fc987e44ec0f9569c
antter5 <57975560+antter5@users.noreply.github.com>
2023-11-04 11:58:45 +0900

    Update 2_1.sql

--- 2_1.sql
+++ 2_1.sql
@@ -4,5 +4,5 @@ WHERE age > 25;
 /*
 나이가 25세보다 많은 조건으로(WHERE age > 25)
 "Customers" 테이블에서(FROM Customers)
-"first_name"과 "country"를 출력한다(SELECT first_name, country)
+"first_name"과 "country" 칼럼을 출력한다(SELECT first_name, country)
 */
