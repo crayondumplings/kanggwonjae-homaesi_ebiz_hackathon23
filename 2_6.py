0996b3590ec83d2cedbe7974858395024c82f9d5
antter5 <57975560+antter5@users.noreply.github.com>
2023-11-04 13:38:38 +0900

    Update 2_6.py

--- 2_6.py
+++ 2_6.py
@@ -1,20 +1,20 @@
+# 파이썬의 re 모듈을 불러온다. 이 모듈은 정규 표현식을 사용하여 문자열을 처리하는 데 사용된다.
 import re
 
+# count_spanish_characters라는 함수를 정의한다. 이 함수는 입력 문자열을 받아서 스페인어 알파벳으로 변환하고, 변환된 문자열의 길이를 반환한다.
 def count_spanish_characters(input_string):
-    # 확장 문자를 원래의 스페인어 알파벳으로 변환
+    # re.sub 함수를 사용하여 입력 문자열에서 확장 문자를 찾아 원래의 스페인어 알파벳으로 변환한다.
+    # 'a,'는 'á', 'e,'는 'é', 'i,'는 'í', 'o,'는 'ó', 'u,'는 'ú', 'n~'는 'ñ', 'u..'는 'ü'로 변환된다.
     input_string = re.sub(r'a,|e,|i,|o,|u,|n~|u\.\.', lambda m: {'a,':'á', 'e,':'é', 'i,':'í', 'o,':'ó', 'u,':'ú', 'n~':'ñ', 'u..':'ü'}[m.group()], input_string)
     
-    # 스페이스를 제외한 문자의 개수를 세기
+    # replace 함수를 사용하여 문자열에서 공백을 제거하고, len 함수를 사용하여 문자열의 길이를 구한다.
+    # 이렇게 하면 스페이스를 제외한 문자의 개수를 얻을 수 있다.
     count = len(input_string.replace(" ", ""))
     
-    return count
+    # 변환된 문자열과 그 길이를 반환한다.
+    return input_string, count
 
 # 예시
-print(count_spanish_characters("espan~afu,tbol"))  # 출력: 12
-print(count_spanish_characters("si,aqui,esta"))  # 출력: 9
-
-# re.sub 함수는 문자열에서 패턴을 찾아 다른 문자열로 대체하는 역할을 한다.
-# 첫 번째 인자인 r'a,|e,|i,|o,|u,|n~|u\.\.'는 찾을 패턴을 나타낸다. 여기서는 확장 문자를 나타내는 문자열들을 찾는다.
-# 두 번째 인자인 lambda m: {'a,':'á', 'e,':'é', 'i,':'í', 'o,':'ó', 'u,':'ú', 'n~':'ñ', 'u..':'ü'}[m.group()]는 찾은 패턴을 어떻게 대체할지를 정의한다. 여기서는 찾은 확장 문자를 해당하는 스페인어 알파벳으로 대체한다.
-# 세 번째 인자인 input_string는 대체 작업을 수행할 원본 문자열이다.
-# 이렇게 하면 input_string에서 확장 문자를 찾아 해당하는 스페인어 알파벳으로 대체한 후, 그 결과를 다시 input_string에 저장한다. 이 과정을 통해 원래의 스페인어 문장을 복원한다.
+# "espan~afu,tbol" 문자열을 변환하고, 변환된 문자열의 길이를 출력한다.
+converted_string, count = count_spanish_characters("espan~afu,tbol")
+print(f"변환된 문자열: {converted_string}, 문자 개수: {count}")
